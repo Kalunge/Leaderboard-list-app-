25a87c9d65d41023a1c76c17c0390ed3a0e3dd4f
kalunge <muthomititus+github@gmail.com>
2021-08-31 11:35:43 +0100

    Sort deployment error

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-(() => {
+/* eslint-disable */ (() => {
   'use strict';
   var e,
     n,
--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,20 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-  devServer: {
-    contentBase: './docs',
-  },
+  entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
-
+  devServer: {
+    contentBase: './docs',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
-
   module: {
     rules: [
       {
